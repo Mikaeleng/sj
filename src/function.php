6a852b8bb9ec6a1558da801815a382087976770e
mikaeleng <engmikael@gmail.com>
2015-10-24 21:12:04 +0200

    fredagsarbetet med viss live grafik i top

--- src/function.php
+++ src/function.php
@@ -1,12 +1,14 @@
 <?php
 //      Top menu elements
+
+
 function addTopMenu(){
         ?>
 <div class="pure-g" id="top-menu">
-    <div class="pure-u-1-2">
+    <div class="pure-u-sm-1-2 pure-u-1-2">
         <span>MENY</span>
     </div>
-    <div class="pure-u-1-2">
+    <div class="pure-u-sm-1-2 pure-u-1-2">
         <span>SÖK</span>
     </div>
 </div>
@@ -23,39 +25,43 @@ function createMainNav(){
 }
 
 function addNewsList(){
-
+$news       = "NYHET";
+$blog       = "BLOG";
+$kpi        = "NYCKELTAL";
+$subscribe  = "ETIKETT";
+$train      = "PÅ TÅGEN";
     ?>
 <div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">NYHET</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-1-5 pure-u-1-5 cat-news"><?php echo $news; ?></div>
+       <div class="pure-u-sm-4-5 pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-1-4 cat-blog">BLOG</div>
-       <div class="pure-u-3-4 cat-caption"> <span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-1-4 pure-u-1-4 cat-blog"><?php echo $blog; ?></div>
+       <div class="pure-u-sm-3-4 pure-u-3-4 cat-caption"> <span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-1-4 cat-blog">BLOG</div>
-       <div class="pure-u-3-4 cat-caption"> <span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-1-4 pure-u-1-4 cat-blog"><?php echo $blog; ?></div>
+       <div class="pure-u-sm-3-4 pure-u-3-4 cat-caption"> <span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">NYHET</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-1-5 pure-u-1-5 cat-news"><?php echo $news; ?></div>
+       <div class="pure-u-sm-4-5 pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-7-24 cat-kpi">NYCKELTAL</div>
-       <div class="pure-u-17-24 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-7-24 pure-u-7-24 cat-kpi"><?php echo $kpi; ?></div>
+       <div class="pure-u-sm-17-24 pure-u-17-24 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-7-24 cat-train">PÅ TÅGEN</div>
-       <div class="pure-u-14-24 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-7-24 pure-u-7-24 cat-train"><?php echo $train; ?></div>
+       <div class="pure-u-sm-14-24 pure-u-14-24 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-7-24 cat-subscribe">ETIKETT</div>
-       <div class="pure-u-16-24 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-7-24 pure-u-7-24 cat-subscribe"><?php echo $subscribe; ?></div>
+       <div class="pure-u-sm-16-24 pure-u-16-24 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">NYHET</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+    <div class="pure-u-sm-1-5 pure-u-1-5 cat-news"><?php echo $news; ?></div>
+       <div class="pure-u-sm-4-5 pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <?php
     /*$cat = array('news'=>'NYHET', 'blog' =>'BLOGG', 'kpi' => 'NYCKELTAL', 'subribe'=>'PRENUMERATION');
@@ -76,29 +82,29 @@ function addNewsList(){
 
 function addWorkList(){
     ?>
-<div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">LÄNK</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+<div class="pure-g work">
+    <div class="pure-u-1-5 work-link">LÄNK</div>
+       <div class="pure-u-4-5 work-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
-<div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">LÄNK</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+<div class="pure-g work">
+    <div class="pure-u-1-5 work-link">LÄNK</div>
+       <div class="pure-u-4-5 work-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
-<div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">LÄNK</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+<div class="pure-g work">
+    <div class="pure-u-1-5 work-link">LÄNK</div>
+       <div class="pure-u-4-5 work-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
-<div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">LÄNK</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+<div class="pure-g work">
+    <div class="pure-u-1-5 work-link">LÄNK</div>
+       <div class="pure-u-4-5 work-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
-<div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">LÄNK</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+<div class="pure-g work">
+    <div class="pure-u-1-5 work-link">LÄNK</div>
+       <div class="pure-u-4-5 work-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
-<div class="pure-g cat">
-    <div class="pure-u-1-5 cat-news">LÄNK</div>
-       <div class="pure-u-4-5 cat-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
+<div class="pure-g work">
+    <div class="pure-u-1-5 work-link">LÄNK</div>
+       <div class="pure-u-4-5 work-caption"><span>Rubrik lorem ipsum dolor sit amet consectetuer</span></div>
 </div>
 <?php
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,106 @@
 
 
+var _winWidth = $( window).width();
+
 jQuery( document ).ready(function( $ ) {
 
-    $( "#news button" ).click(function() {
-        console.info('clicked visa fler button');
+    toggleElements();
+
+    $( window ).resize(function() {
+
+        _winWidth = $( window).width();
+       toggleElements();
+
     });
 
-    $( "#news-tab" ).click(function() {
-        $( "#news-tab" ).addClass('active-tab');
-        $( "#work-tab" ).removeClass('active-tab');
 
-        $( "#work" ).slideUp( "fast", function() {
+    $( "#news-tab" ).click(function(event) {
+    if(_winWidth<568) {
+        event.preventDefault();
+        $("#sort-menu").removeClass("hidden");
+        $("#sort-items").addClass("hidden");
+
+        // toggle bacground color on buttons
+        $("#news-tab").addClass('active-tab');
+        $("#work-tab").removeClass('active-tab');
+
+        // shows/hides green arrow
+        $("#right-arrow img").addClass('hidden');
+        $("#left-arrow img").removeClass('hidden');
+
+        // animates the lists show/hidden
+        $("#work").slideUp("fast", function () {
             // Animation complete.
         });
-        $("#news" ).slideDown( "fast", function() {
+        $("#news").slideDown("fast", function () {
             // Animation complete.
         });
+    }
     });
 
-    $( "#work-tab" ).click(function() {
-        $( "#news-tab" ).removeClass('active-tab');
+    $( "#work-tab" ).click(function(event) {
+        if(_winWidth<568) {
+            event.preventDefault();
+            $("#sort-menu").removeClass("hidden");
+            $("#sort-items").addClass("hidden");
 
-        $( "#work" ).removeClass('hidden');
-        $( "#work-tab" ).addClass('active-tab');
+            $("#news-tab").removeClass('active-tab');
 
-        $( "#news" ).slideUp( "fast", function() {
-            // Animation complete.
+            $("#work").removeClass('hidden');
+            $("#work-tab").addClass('active-tab');
+
+            // shows/hides green arrow
+            $("#left-arrow img").addClass('hidden');
+            $("#right-arrow img").removeClass('hidden');
+
+            $("#news").slideUp("fast", function () {
+                // Animation complete.
+            });
+            $("#work").slideDown("fast", function () {
+                // Animation complete.
+            });
+        }
         });
-        $("#work" ).slideDown( "fast", function() {
-            // Animation complete.
-        });
+});
+
+$( "#button-sort" ).click(function() {
+        $("#sort-menu").addClass("hidden");
+        $("#sort-items").removeClass("hidden");
+});
+
+$( "#sort-items button" ).click(function(event) {
+    event.preventDefault();
+    $("#sort-menu").removeClass("hidden");
+    $("#sort-items").addClass("hidden");
+});
+
+
+$( ".cat-news, .cat-blog, .cat-subscribe, .cat-train, .cat-kpi" ).click(function(event) {
+    event.preventDefault();
+    $("#resetList-button").removeClass("hidden");
+    var _class = event.target.classList[2];
+    $( ".cat" ).each(function( index ) {
+        if($(this).children().hasClass(_class)==false){
+            $(this).addClass("hidden");
+        }
+    });102
+});
+
+$( "#resetList-button" ).click(function(event) {
+    event.preventDefault();
+    $(this).addClass("hidden");
+    $( ".cat" ).each(function( index ) {
+        $(this).removeClass("hidden");
     });
 });
+
+
+function toggleElements(){
+    if(_winWidth>1024){
+        $("#work").removeClass("hidden");
+    }else if(_winWidth<1024){
+        $("#work").addClass("hidden");
+
+    }
+}
+
